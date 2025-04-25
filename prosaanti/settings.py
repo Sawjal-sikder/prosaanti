934ad95ce308c6e4fc1e08c216e698ff140347d5
Sawjal-sikder <mdsajalsikder@gmail.com>
2025-04-26 00:45:44 +0600

    Add navbar section static

--- prosaanti/settings.py
+++ prosaanti/settings.py
@@ -1,4 +1,6 @@
 from pathlib import Path
+import os
+
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -91,9 +93,12 @@ USE_I18N = True
 USE_TZ = True
 
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+# STATICFILES_DIRS = [
+#     os.path.join(BASE_DIR, 'static'),  
+# ]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles') 
 MEDIA_URL = 'media/'
-STATICFILES_DIRS = [BASE_DIR / "static"]
 MEDIA_ROOT = BASE_DIR / 'media'
 
 
